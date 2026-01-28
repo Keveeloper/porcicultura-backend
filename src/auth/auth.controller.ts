bc2ed3fa050228c11f28dba890022a6b72027f30
Profesional digital <profesionaldigital@impormedical.com.co>
2026-01-28 17:39:43 -0500

    API /auth/me and /auth/logout implemented successfuly

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Post, Headers, UseInterceptors, ClassSerializerInterceptor, UnauthorizedException, Inject, Res } from '@nestjs/common';
-// import * as admin from 'firebase-admin';
-// import { UsersService } from '../users/users.service';
-// import { JwtService } from '@nestjs/jwt';
+import { Controller, Post, Headers, UseInterceptors, ClassSerializerInterceptor, Res, Get, UseGuards } from '@nestjs/common';
 import type { Response } from 'express';
 import { AuthService } from './auth.service';
+import { CurrentUser } from 'src/common/decorators/current-user.decorator';
+import { User } from 'src/users/entities/user.entity';
+import { UsersService } from 'src/users/users.service';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Controller('auth')
 @UseInterceptors(ClassSerializerInterceptor)
@@ -11,10 +12,19 @@ export class AuthController {
 
   constructor(
     private readonly authService: AuthService,
-    // private readonly usersService: UsersService,
-    // private readonly jwtService: JwtService,
-    // @Inject('FIREBASE_ADMIN') private readonly firebaseAdmin: admin.app.App,
+    private readonly userService: UsersService,
   ) {}
+
+  /**
+   * @Description: Endpoint para obtener los datos del usuario autenticado.
+   * @param user
+   * @returns
+   */
+  @Get('me')
+  @UseGuards(JwtAuthGuard)
+  async getMe(@CurrentUser() user: User){
+    return await this.userService.getUserById(user.id);
+  }
 
   /**
    * Description:         Endpoint para manejar el inicio de sesión con Google/Firebase.
@@ -38,57 +48,22 @@ export class AuthController {
     });
 
     return user;
-    //Modify porcicultura software
-    // if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    //   throw new UnauthorizedException('Token de autorización no proporcionado o formato inválido.');
-    // }
 
-    // const firebaseIdToken = authHeader.split(' ')[1];
-
-    // try {
-    //   // 2. Verificar el Token de ID de Firebase
-    //   const decodedToken = await this.firebaseAdmin.auth().verifyIdToken(firebaseIdToken);
-    //   const { uid, email, name, picture } = decodedToken;
-
-    //   const [firstName, lastName] = name ? name.split(' ') : ['Usuario', 'Google'];
-
-    //   const userResponse = await this.usersService.findOrCreate({
-    //     uid,
-    //     email: email || '',
-    //     password: '',
-    //     profile: {
-    //       firstName: firstName || '',
-    //       lastName: lastName || '',
-    //       avatar: picture,
-    //     },
-    //   });
-
-    //   const payload = {
-    //     sub: userResponse.id,
-    //     email: userResponse.email
-    //   };
-
-    //   const nestJsToken = this.jwtService.sign(payload);
-
-    //   response.cookie('accessToken', nestJsToken, {
-    //     httpOnly: true,    // Prevents JS access (XSS protection)
-    //     secure: false,     // Set to true in production with HTTPS
-    //     sameSite: 'lax',   // Helps against CSRF
-    //     maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days in milliseconds
-    //   });
-
-    //   return {
-    //     userResponse,
-    //     // accessToken: nestJsToken,
-    //   };
-
-    // } catch (error) {
-    //   // Los errores de token inválido o expirado se capturan aquí
-    //   const errorMessage = error.code === 'auth/argument-error' ?
-    //     'Token de Firebase mal formado o ausente.' :
-    //     'Token de Firebase inválido o expirado.';
-
-    //   throw new UnauthorizedException(errorMessage);
-    // }
   }
+
+  /**
+   * @Description: Endpoint para cerrar sesión del usuario.
+   * @param response
+   */
+  @Post('logout')
+  logout(@Res({ passthrough: true }) response: Response) {
+    response.clearCookie('accessToken', {
+      httpOnly: true,
+      secure: false,
+      sameSite: 'lax',
+    });
+
+    return { message: 'Logged out successfully' };
+  }
+
 }
