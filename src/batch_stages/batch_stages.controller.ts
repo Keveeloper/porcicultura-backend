466d9de85156d37ca10f89cbb134e94748b21c3f
Profesional digital <profesionaldigital@impormedical.com.co>
2026-02-15 01:32:52 -0500

    Create batch with JWT implemented 01

--- src/batch_stages/batch_stages.controller.ts
+++ src/batch_stages/batch_stages.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { BatchStagesService } from './batch_stages.service';
 import { CreateBatchStageDto } from './dto/create-batch_stage.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('batch-stages')
+@UseGuards(JwtAuthGuard)
 export class BatchStagesController {
   constructor(private readonly batchStagesService: BatchStagesService) {}
 
